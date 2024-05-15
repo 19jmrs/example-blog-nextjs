f5d6e9ccd4f24e1fb2dc749a435f96de89313b55
Jorge <jorgesa.work@gmail.com>
2024-05-15 21:56:19 +0100

    added velite for content managemen

---
+++ velite.config.ts
@@ -0,0 +1,59 @@
+import { defineConfig, defineCollection, s } from "velite";
+
+// `s` is extended from Zod with some custom schemas,
+// you can also import re-exported `z` from `velite` if you don't need these extension schemas.
+
+// slug: //blog/hello-world and we want the last part
+
+const computedFields = <T extends { slug: string }>(data: T) => ({
+  ...data,
+  slugAsParams: data.slug.split("/").slice(1).join("/"),
+});
+
+const posts = defineCollection({
+  name: "Post",
+  pattern: "blog/**/*.mdx",
+  schema: s
+    .object({
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(999).optional(),
+      date: s.isodate(),
+      published: s.boolean().default(true),
+      image: s.string().max(99),
+      authors: s.array(s.string()),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
+});
+
+const authors = defineCollection({
+  name: "Author",
+  pattern: "authors/**/*.mdx",
+  schema: s
+    .object({
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(999).optional(),
+      avatar: s.string().max(99),
+      twitter: s.string().max(99),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
+});
+
+export default defineConfig({
+  root: "content",
+  output: {
+    data: ".velite",
+    assets: "public/static",
+    base: "/static/",
+    name: "[name]-[hash:6].[ext]",
+    clean: true,
+  },
+  collections: { posts, authors },
+  mdx: {
+    rehypePlugins: [],
+    remarkPlugins: [],
+  },
+});
